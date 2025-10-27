bb5d123076a8c397301b50c523dc7b8242d19c47
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-10-27 22:00:07 +0500

    Add ThemeToggle component and integrate it into HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "@/components/ui/ThemeToggle";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 export default function HomePage() {
   return (
     <div className="flex h-screen items-center justify-center w-screen">
       <SignInButton />
       <UserButton />
+      <ThemeToggle />
     </div>
   );
 }
