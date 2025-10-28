2851144e33a3de5db1cd318b07a2b7dca2c78ed5
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-10-28 19:43:32 +0500

    feat: add database schema and environment configuration for PostgreSQL

---
+++ drizzle/schema/question.ts
@@ -0,0 +1,32 @@
+import { pgEnum, pgTable, uuid } from "drizzle-orm/pg-core";
+import { createdAt, id, updatedAt } from "../schemaHelpers";
+import { varchar } from "drizzle-orm/pg-core";
+import { JobInfoTable } from "./jobInfo";
+import { relations } from "drizzle-orm";
+
+export const questionDifficulties = ["easy", "medium", "hard"] as const;
+
+export type QuestionDifficulty = (typeof questionDifficulties)[number];
+
+export const questionDifficultyEnum = pgEnum(
+  "questions_question_difficulty",
+  questionDifficulties
+);
+
+export const QuestionTable = pgTable("questions", {
+  id,
+  jobInfoId: uuid()
+    .references(() => JobInfoTable.id, { onDelete: "cascade" })
+    .notNull(),
+  text: varchar().notNull(),
+  difficulty: questionDifficultyEnum().notNull(),
+  createdAt,
+  updatedAt,
+});
+
+export const questionRelations = relations(QuestionTable, ({ one }) => ({
+  jobInfo: one(JobInfoTable, {
+    fields: [QuestionTable.jobInfoId],
+    references: [JobInfoTable.id],
+  }),
+}));
